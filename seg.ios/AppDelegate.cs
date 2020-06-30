29fc2ddf237e765580339a45ff6bfc7caa1dd976
Clinton Landry <me@ctlandry.com>
2020-06-30 10:50:05 -0700

    initial

---
+++ seg.ios/AppDelegate.cs
@@ -0,0 +1,23 @@
+﻿using UIKit;
+using Foundation;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using seg.core;
+
+namespace seg.ios
+{
+	[Register("AppDelegate")]
+	public partial class AppDelegate : FormsApplicationDelegate
+	{
+		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+		{
+			// affects all UISwitch controls in the app
+			UISwitch.Appearance.OnTintColor = UIColor.FromRGB(0x91, 0xCA, 0x47);
+
+			Forms.Init();
+			LoadApplication(new App());
+			return base.FinishedLaunching(app, options);
+		}
+	}
+}
+
