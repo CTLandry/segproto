77068af91678d3e593fd94304c12289eab7abb2d
Clinton Landry <me@ctlandry.com>
2020-07-02 10:33:42 -0700

    some fixes for init

--- seg.ios/AppDelegate.cs
+++ seg.ios/AppDelegate.cs
@@ -6,18 +6,26 @@ using seg.core;
 
 namespace seg.ios
 {
-	[Register("AppDelegate")]
-	public partial class AppDelegate : FormsApplicationDelegate
-	{
-		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
-		{
-			// affects all UISwitch controls in the app
-			UISwitch.Appearance.OnTintColor = UIColor.FromRGB(0x91, 0xCA, 0x47);
+    // The UIApplicationDelegate for the application. This class is responsible for launching the 
+    // User Interface of the application, as well as listening (and optionally responding) to 
+    // application events from iOS.
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        //
+        // This method is invoked when the application has loaded and is ready to run. In this 
+        // method you should instantiate the window, load the UI into it and then make the window
+        // visible.
+        //
+        // You have 17 seconds to return from this method, or iOS will terminate your application.
+        //
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            global::Xamarin.Forms.Forms.Init();
+            LoadApplication(new App());
 
-			Forms.Init();
-			LoadApplication(new App());
-			return base.FinishedLaunching(app, options);
-		}
-	}
+            return base.FinishedLaunching(app, options);
+        }
+    }
 }
 
